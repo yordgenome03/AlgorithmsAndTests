ac41b5d34d489ec57c18f5a0a3806a6587500ed6
tahara yo <tahara.you@ring-tech.jp>
2022-12-16 23:00:44 +0900

    追記

--- AlgorithmsAndTests/ViewController.swift
+++ AlgorithmsAndTests/ViewController.swift
@@ -6,14 +6,106 @@
 //
 
 import UIKit
+import Dispatch
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    // 線形探索法（リニアサーチ）
+    func linearSearch<T: Equatable>(_ array: [T], key: T) -> Int? {
+        for i in 0 ..< array.count {
+            if array[i] == key {
+                return i
+            }
+        }
+        return nil
+    }
+    
+    // 二分探索法（バイナリーサーチ）
+    // 引数arrayに渡す配列はソート済みのこと
+    // 引数rangeには最初 0..<array.count を渡す
+    func binarySearch<T: Comparable>(_ array: [T], key: T, range: Range<Int>) -> Int? {
+        if range.lowerBound >= range.upperBound {
+            return nil
+        } else {
+            let middle = range.lowerBound + (range.upperBound - range.lowerBound)/2
+            // arrat[middle]がkeyより左にある時は、求めるindexはmiddleより右にあるため、左側の範囲をmiddle + 1にして詰めて再起処理
+            if array[middle] < key {
+                return binarySearch(array, key: key, range: middle + 1 ..< range.upperBound)
+                
+                // arrat[middle]がkeyより右にある時は、求めるindexはmiddleより左にあるため、右側の範囲をmiddleにして詰めて再起処理
+            } else if array[middle] > key {
+                return binarySearch(array, key: key, range: range.lowerBound ..< middle)
+            } else {
+                return middle
+            }
+        }
+    }
+    
+    var nums = [5, 3, 7, 9, 1, 2, 4, 8, 6]
+
+    // 単純交換法（バブルソート）：昇順
+    func bubbleSort<T: Comparable>(_ array: [T]) -> [T] {
+        var sortedArray = array
+        let n = array.count
+        // リストの先頭から値を比較していき昇順に並べる
+        // 一番内側のループが終了した時、最大値がリストの終端に移動する
+        for i in 0..<n {
+            // 次のループではリストの要素数を一つ減らして比較
+            for j in 0..<(n-i-1) {
+                if sortedArray[j] > sortedArray[j+1] {
+                    sortedArray.swapAt(j, j+1)
+                }
+            }
+        }
+        return sortedArray
+    }
+
+    // 単純選択法（選択ソート）：昇順
+    func selectionSort<T: Comparable>(_ array: [T]) -> [T] {
+        if array.count <= 1 { return array }
+        var sortedArray = array
+        let n = array.count
+        // array[i]~array[n-1]で最小値を見つけてarray[i]と交換、array[i]は整備済みに
+        for i in 0..<n {
+            var lowest = i
+            // array[i+1]~array[n-1]で最小値を見つけてarray[i+1]と交換、array[i+1]は整備済みに
+            for j in (i+1)..<n {
+                // 最小値を見つける度lowestの値（インデックス番号）を更新
+                if sortedArray[j] < sortedArray[lowest] {
+                    lowest = j
+                }
+            }
+            // lowestがiじゃなかったらarray[i]の位置にarray[lowest]を交換
+            if i != lowest {
+                sortedArray.swapAt(i, lowest)
+            }
+        }
+        return sortedArray
+    }
+    
+    // 単純挿入法（挿入ソート）
+    func insertionSort<T: Comparable>(_ array: [T]) -> [T] {
+        var sortedArray = array
+        let n = array.count
+        // index番号1までを整列したらindex番号2まで,それが整列できたら3まで,4まで...と整列させていく
+        for index in 1..<n {
+            var currentIndex = index
+            // currentIndexが0になるまで、array[i-1] < array[i]となるように並べ替え続ける
+            while currentIndex > 0 && sortedArray[currentIndex] < sortedArray[currentIndex - 1] {
+                sortedArray.swapAt(currentIndex - 1, currentIndex)
+                currentIndex -= 1
+            }
+        }
+        return sortedArray
+    }
+    
+    
 
 }
+
 
--- AlgorithmsAndTestsTests/AlgorithmsAndTestsTests.swift
+++ AlgorithmsAndTestsTests/AlgorithmsAndTestsTests.swift
@@ -9,28 +9,93 @@ import XCTest
 @testable import AlgorithmsAndTests
 
 final class AlgorithmsAndTestsTests: XCTestCase {
+    override class func setUp() {
+        super.setUp()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        print("テストケース全体の事前処理")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override class func tearDown() {
+        super.tearDown()
+        
+        print("テストケース全体の事後処理")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    override func setUp() {
+        super.setUp()
+        
+        print("テスト毎の事前処理")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        print("テスト毎の事後処理")
     }
-
+    
+    func testLinearSearch() {
+        print(#function)
+        let nums = [1, 3, 4, 6, 8, 11, 14, 15, 18, 19, 23, 25, 26, 29]
+        let vc = ViewController()
+        
+        XCTAssertEqual(vc.linearSearch(nums, key: 4), 2)
+        XCTAssertEqual(vc.linearSearch(nums, key: 8), 4)
+        XCTAssertEqual(vc.linearSearch(nums, key: 11), 5)
+        XCTAssertEqual(vc.linearSearch(nums, key: 12), nil)
+    }
+ 
+    
+    func testBinarySearch() {
+        print(#function)
+        let nums = [1, 3, 4, 6, 8, 11, 14, 15, 18, 19, 23, 25, 26, 29]
+        let vc = ViewController()
+        
+        XCTAssertEqual(vc.binarySearch(nums, key: 3, range: 0..<nums.count), 1)
+        XCTAssertEqual(vc.binarySearch(nums, key: 2, range: 0..<nums.count), nil)
+        XCTAssertEqual(vc.binarySearch(nums, key: 29, range: 0..<nums.count), 13)
+        XCTAssertEqual(vc.binarySearch(nums, key: 30, range: 0..<nums.count), nil)
+    }
+    
+//    func testBubbleSort() {
+//        print(#function)
+//        let nums1 = [5, 3, 7, 9, 1, 2, 4, 8, 6]
+//        let nums2 = [5]
+//        let nums3 = [Int]()
+//        let nums4 = [1.1, 500.5, 30.3, 7.7, 99.9]
+//        let vc = ViewController()
+//
+//        XCTAssertEqual(vc.bubbleSort(nums1), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+//        XCTAssertEqual(vc.bubbleSort(nums2), [5])
+//        XCTAssertEqual(vc.bubbleSort(nums3), nums3)
+//        XCTAssertEqual(vc.bubbleSort(nums4), [1.1, 7.7, 30.3, 99.9, 500.5])
+//
+//    }
+//
+//    func testSelectionSort() {
+//        print(#function)
+//        let nums1 = [5, 3, 7, 9, 1, 2, 4, 8, 6]
+//        let nums2 = [5]
+//        let nums3 = [Int]()
+//        let nums4 = [1.1, 500.5, 30.3, 7.7, 99.9]
+//        let vc = ViewController()
+//
+//        XCTAssertEqual(vc.selectionSort(nums1), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+//        XCTAssertEqual(vc.selectionSort(nums2), [5])
+//        XCTAssertEqual(vc.selectionSort(nums3), nums3)
+//        XCTAssertEqual(vc.selectionSort(nums4), [1.1, 7.7, 30.3, 99.9, 500.5])
+//    }
+//
+//    func testInseertionIndex() {
+//        print(#function)
+//        let nums1 = [5, 3, 7, 9, 1, 2, 4, 8, 6]
+//        let nums2 = [5]
+//        let nums3 = [Int]()
+//        let nums4 = [1.1, 500.5, 30.3, 7.7, 99.9]
+//        let vc = ViewController()
+//
+//        XCTAssertEqual(vc.insertionSort(nums1), [9, 8, 7, 6, 5, 4, 3, 2, 1])
+//        XCTAssertEqual(vc.insertionSort(nums2), [5])
+//        XCTAssertEqual(vc.insertionSort(nums3), nums3)
+//        XCTAssertEqual(vc.insertionSort(nums4), [500.5, 99.9, 30.3, 7.7, 1.1])
+//    }
 }
